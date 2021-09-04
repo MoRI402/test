7ec774fb092e655ca58ba0fbb6b6633d3b905150
MoRI402 <79321201+MoRI402@users.noreply.github.com>
2021-09-04 19:55:46 +0530

    Update animeoceansearch.py

--- animeoceansearch.py
+++ animeoceansearch.py
@@ -1,13 +1,13 @@
 from . import *
 from telethon import events
 
-chat = 1152077444
+chat = 1202167338
 
 @asst.on(events.NewMessage(pattern="\\/search ?(.*)"))
 async def _(e):
     key = e.pattern_match.group(1)
     txt = ""
     async for x in bot.iter_messages(chat, search=key, reverse=True):
-        txt += f"https://t.me/anime_ocean/{x.id}\n"
+        txt += f"https://t.me/Animax_Industry/{x.id}\n"
     if txt:
         await e.reply(txt, link_preview=False,)
