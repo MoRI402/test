6262acf47e3f28b37e3a151ae90f93f3b6ac6b89
MoRI402 <79321201+MoRI402@users.noreply.github.com>
2021-09-04 19:56:47 +0530

    Update oceansearch.py

--- oceansearch.py
+++ oceansearch.py
@@ -1,13 +1,13 @@
 from . import *
 from telethon import events
 
-chat = 1152077444
+chat = 1202167338
 
-@asst.on(events.NewMessage(pattern="\\/search@copyninja2_bot ?(.*)"))
+@asst.on(events.NewMessage(pattern="\\/search@Siddharth_Otaku_bot ?(.*)"))
 async def _(e):
     key = e.pattern_match.group(1)
     txt = ""
     async for x in bot.iter_messages(chat, search=key, reverse=True):
-        txt += f"https://t.me/anime_ocean/{x.id}\n"
+        txt += f"https://t.me/animax_industry/{x.id}\n"
     if txt:
         await e.reply(txt, link_preview=False,)
