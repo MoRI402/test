459a2e76e49d716f23caefc3e63a35c63113827d
MoRI402 <79321201+MoRI402@users.noreply.github.com>
2021-09-04 19:51:54 +0530

    Add files via upload

---
+++ animeoceansearch.py
@@ -0,0 +1,13 @@
+from . import *
+from telethon import events
+
+chat = 1152077444
+
+@asst.on(events.NewMessage(pattern="\\/search ?(.*)"))
+async def _(e):
+    key = e.pattern_match.group(1)
+    txt = ""
+    async for x in bot.iter_messages(chat, search=key, reverse=True):
+        txt += f"https://t.me/anime_ocean/{x.id}\n"
+    if txt:
+        await e.reply(txt, link_preview=False,)
